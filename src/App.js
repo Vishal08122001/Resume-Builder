443c17975b2d95e62f96ab298d0d1bbefc3af5fd
Vishal08122001 <vc31142@gmail.com>
2023-01-08 20:00:41 +0530

    hp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home"
 import CheckSelectedId from './Components/CheckSelectedId';
-import FillDetails from "./Pages/fill_Details"
+import FillDetails from "./Pages/FillDetails"
 import About from "./Pages/About"
 import MyResume from "./Pages/MyResumes"
 
